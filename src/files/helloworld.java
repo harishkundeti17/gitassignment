f224d4d17c80c2cadada98781d73b553a7d81f1e
Harish Kundeti <kundetiharish17@gmail.com>
2022-12-27 22:41:02 +0530

    first commit

---
+++ src/files/helloworld.java
@@ -0,0 +1,11 @@
+package files;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello");
+     System.out.println("world");
+	}
+
+}
---
+++ src/files/multipleoftwonumbers.java
@@ -0,0 +1,12 @@
+package files;
+
+public class multipleoftwonumbers {
+	public static void main(String args[])
+	{
+	int x = 5;
+	int y = 6;
+	int mul = x * y;
+	System.out.println(mul); 
+	}
+
+}
